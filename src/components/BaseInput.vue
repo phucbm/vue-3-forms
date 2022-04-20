88976fb2564733b3a4e54f8e28c3364d8f692445
phucbm <hiphucbui@gmail.com>
2022-04-20 21:49:57 +0700

    :bento: BaseInput

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <label>{{ label }}</label>
+  <input
+      v-bind="$attrs"
+      :placeholder="label"
+      class="field"
+      :value="modelValue"
+      @input="$emit('update:modelValue',$event.target.value)"
+  >
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -6,39 +6,35 @@
       <label>Select a category</label>
       <select v-model="event.category">
         <option
-          v-for="option in categories"
-          :value="option"
-          :key="option"
-          :selected="option === event.category"
-        >{{ option }}</option>
+            v-for="option in categories"
+            :value="option"
+            :key="option"
+            :selected="option === event.category"
+        >{{ option }}
+        </option>
       </select>
 
       <h3>Name & describe your event</h3>
 
-      <label>Title</label>
-      <input
-        v-model="event.title"
-        type="text"
-        placeholder="Title"
-        class="field"
-      >
 
-      <label>Description</label>
-      <input
-        v-model="event.description"
-        type="text"
-        placeholder="Description"
-        class="field"
+      <BaseInput
+          v-model="event.title"
+          label="Title"
+          type="text"
+      />
+
+      <BaseInput
+          v-model="event.description"
+          label="Description"
+          type="text"
       />
 
       <h3>Where is your event?</h3>
 
-      <label>Location</label>
-      <input
-        v-model="event.location"
-        type="text"
-        placeholder="Location"
-        class="field"
+      <BaseInput
+          v-model="event.location"
+          label="Location"
+          type="text"
       />
 
       <h3>Are pets allowed?</h3>
@@ -48,16 +44,16 @@
             v-model="event.pets"
             :value="1"
             name="pets"
-          />
+        />
         <label>Yes</label>
       </div>
 
       <div>
         <input
-          type="radio"
-          v-model="event.pets"
-          :value="0"
-          name="pets"
+            type="radio"
+            v-model="event.pets"
+            :value="0"
+            name="pets"
         />
         <label>No</label>
       </div>
@@ -65,18 +61,18 @@
       <h3>Extras</h3>
       <div>
         <input
-          type="checkbox"
-          v-model="event.extras.catering"
-          class="field"
+            type="checkbox"
+            v-model="event.extras.catering"
+            class="field"
         />
         <label>Catering</label>
       </div>
 
       <div>
         <input
-          type="checkbox"
-          v-model="event.extras.music"
-          class="field"
+            type="checkbox"
+            v-model="event.extras.music"
+            class="field"
         />
         <label>Live music</label>
       </div>
@@ -87,8 +83,11 @@
 </template>
 
 <script>
+import BaseInput from "@/components/BaseInput";
+
 export default {
-  data () {
+  components: {BaseInput},
+  data(){
     return {
       categories: [
         'sustainability',
